07dc5fc7c63d5cf3de99b4b8e6f7d5691e2acb88
Scc_hy <34824890+scchy@users.noreply.github.com>
2021-01-31 15:01:14 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,9 +2,23 @@
 # author: Scc_hy 
 # create date: 2019-10-12
 # Function： 遍历目录
+# --------------------------
+# revise date: 2021-01-31
+#         tip: add logger
 
+# -------------------------------
 
+import logging
 import sys, os
+
+logging.basicConfig(
+    format='%(asctime)s %(name)s %(levelname)s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+logger.info('hello')
+
 
 def lister(root): #对于根目录
     for dirname, subshere, fileshere in os.walk(root):
